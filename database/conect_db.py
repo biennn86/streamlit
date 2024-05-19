75087e6423e80ec28a565b91894a71a48ccb6a95
biennn86 <biennn86@gmail.com>
2024-05-19 21:12:43 +0700

    Update conect_db.py

--- database/conect_db.py
+++ database/conect_db.py
@@ -11,6 +11,6 @@ class ConnectDB:
         except:
             print('Connect Database Fail.')
           
-version https://git-lfs.github.com/spec/v1
-oid sha256:bb5e3c533a4ea05e1d7067e90e4fd343596f3fa2cec4eda5818a5bec0285da0f
-size 346
+# version https://git-lfs.github.com/spec/v1
+# oid sha256:bb5e3c533a4ea05e1d7067e90e4fd343596f3fa2cec4eda5818a5bec0285da0f
+# size 346
