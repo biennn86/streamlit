2596702fe3c11e0dfaaff31b208b5b4a8c86a076
biennn86 <biennn86@gmail.com>
2024-05-19 21:11:45 +0700

    Update conect_db.py

--- database/conect_db.py
+++ database/conect_db.py
@@ -1,3 +1,16 @@
+import sqlite3
+
+class ConnectDB:
+    __NAME_DB = 'database/db_trans_rtcis.db'
+
+    def getConection(self):
+        try:
+            self.conn = sqlite3.connect(self.__NAME_DB)
+            print('Kết nối database thành công')
+            return self.conn
+        except:
+            print('Connect Database Fail.')
+          
 version https://git-lfs.github.com/spec/v1
 oid sha256:bb5e3c533a4ea05e1d7067e90e4fd343596f3fa2cec4eda5818a5bec0285da0f
 size 346
